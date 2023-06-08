bf86c3a6708a17da43ab5700900a98bb5bee3429
frostime <frostime@foxmail.com>
2023-06-08 19:32:46 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-import { Plugin, fetchPost } from "siyuan";
+import { Plugin } from "siyuan";
+import { getFile } from "./utils";
 
 export async function track(plugin: Plugin) {
     let pluginName = plugin.name;
     return pluginName;
 }
+
+
+const StorageName = 'PluginVersion';
+
+async function checkPluginVersion(plugin: Plugin) {
+    const pluginJsonPath = `/data/plugins/${plugin.name}/plugin.json`
+    try {
+        let plugin_file = await getFile(pluginJsonPath);
+        if (plugin_file === null) {
+            return;
+        }
+        plugin_file = JSON.parse(plugin_file);
+        let version = plugin_file.version;
+        console.log(`${plugin.name} Ver: ${version}`);
+
+        let oldVer = plugin.data[StorageName];
+
+        //发现更新到了不同的版本
+        if (version !== oldVer) {
+            plugin.data[StorageName] = version;
+            plugin.saveData(StorageName, version);
+            // showChangeLog(this.version);
+        }
+    } catch (error_msg) {
+        console.error(`Setting load error: ${error_msg}`);
+    }
+}
+
---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+async function myFetchSyncPost(url: string, data: any) {
+    const init: RequestInit = {
+        method: "POST",
+    };
+    if (data) {
+        init.body = JSON.stringify(data);
+    }
+    const res = await fetch(url, init);
+    const txt = await res.text();
+    return txt;
+}
+
+/**
+ * 使用了自定义的 fetchSyncPost
+ * @param path
+ * @returns 返回原始的文本 txt
+ */
+export async function getFile(path: string): Promise<any> {
+    let data = {
+        path: path
+    }
+    let url = '/api/file/getFile';
+    try {
+        let file = await myFetchSyncPost(url, data);
+        return file;
+    } catch (error_msg) {
+        return null;
+    }
+}
