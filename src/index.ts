7ea7948c9355abd01e403ab77f06c4e00d8db813
frostime <frostime@foxmail.com>
2023-06-08 20:36:39 +0800

    feat: 完成了基本框架

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,45 @@
-import { Plugin } from "siyuan";
-import { getFile } from "./utils";
+import { Plugin, Dialog } from "siyuan";
+import { getFile, showChangeLog } from "./utils";
 
 const StorageName = 'PluginVersion';
 
-export async function track(plugin: Plugin) {
+interface TrackResult {
+    VersionChanged: boolean | undefined;
+    OldVersion: string | undefined;
+    NewVersion: string | undefined;
+    Dialog: Dialog | undefined;
+}
+
+export async function track(plugin: Plugin): Promise<TrackResult> {
     const pluginJsonPath = `/data/plugins/${plugin.name}/plugin.json`
+    let result: TrackResult = {
+        VersionChanged: undefined,
+        OldVersion: undefined,
+        NewVersion: undefined,
+        Dialog: undefined
+    }
     try {
         let pluginFile = await getFile(pluginJsonPath);
         if (pluginFile === null) {
-            return;
+            return result;
         }
         pluginFile = JSON.parse(pluginFile);
         let version = pluginFile.version;
         console.log(`${plugin.name} Ver: ${version}`);
 
         let oldVer = await plugin.loadData(StorageName);
+        result.OldVersion = oldVer;
+        result.NewVersion = version;
 
-        //发现更新到了不同的版本
+        //Another version found
         if (version !== oldVer) {
+            result.VersionChanged = true;
             console.log(`${plugin.name} got new version: ${oldVer} -> ${version}`);
             plugin.saveData(StorageName, version);
-            // showChangeLog(this.version);
+            result.Dialog = await showChangeLog(plugin.name, version);
         }
     } catch (error_msg) {
         console.error(`Setting load error: ${error_msg}`);
     }
+    return result;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Dialog } from "siyuan";
+
 async function myFetchSyncPost(url: string, data: any) {
     const init: RequestInit = {
         method: "POST",
@@ -27,3 +29,50 @@ export async function getFile(path: string): Promise<any> {
         return null;
     }
 }
+
+function showTypoDialog(title: string, typo: string, width?: string) {
+    return new Dialog({
+        title: title,
+        content: `
+        <div id="dialog" class="b3-typography" style="margin: 2rem; font-size: 1rem">
+            ${typo}
+        </div>`,
+        width: width,
+        height: "50%"
+    });
+}
+
+export async function showChangeLog(pluginName: string, version: string): Promise<Dialog|undefined> {
+    try {
+        //从 version 版本号中提取主要版本号 mainVersion，比如 1.1.1-beta 或 1.1.1.patch 等，都提取出 1.1.1
+        let match = version.match(/\d+\.\d+\.\d+/g);
+        if (match === null) {
+            console.log(`版本号格式不正确：${version}`);
+            return;
+        }
+        let mainVersion = match[0];
+
+        //TODO 解决 i18n 问题
+        const path = `/data/plugins/${pluginName}/i18n/${i18n.ChangeLog.file}-${mainVersion}.md`;
+
+        let file: string = await getFile(path);
+        let code404 = file.match(/"code":404/g);
+        if (code404 !== null) {
+            console.log(`找不到更新文件：${path}`);
+            return;
+        }
+
+        //@ts-ignore
+        const lute = window.Lute!.New();
+
+        let content = lute.Md2HTML(file);
+        return showTypoDialog(
+            `${pluginName} v${version}`,
+            content,
+            "60%"
+        );
+    } catch (err) {
+        console.log('showChangeLog error:', err);
+    }
+}
+
